d483b50ecd29513dfe40e0fb2b1bac52a223c346
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-13 16:26:20 -0400

    moved pages to src

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
-import Username from '../components/Username';
-import styles from '../styles/Home.module.css';
+import Username from '../../components/Username';
+import styles from '../../styles/Home.module.css';
 
 interface Comment {
   id: number;
