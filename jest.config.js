629a473f4b62afd15d4282870b7c598971f1b3fa
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-14 12:08:10 -0400

    configured test reporter and scripts to open

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,17 @@ module.exports = {
     // Resolve .jpg and similar files to __mocks__/file-mock.js
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
   },
-  reporters: ['default', ['jest-junit', { outputName: 'coverage/jest-junit/junit.xml' }]],
+  reporters: [
+    'default',
+    ['jest-junit', { outputName: 'coverage/jest-junit/junit.xml' }],
+    [
+      'jest-html-reporters',
+      {
+        publicPath: './html-report',
+        filename: 'report.html',
+        expand: true,
+        // openReport: true,
+      },
+    ],
+  ],
 };
