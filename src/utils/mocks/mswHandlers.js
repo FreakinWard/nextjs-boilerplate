db7eba56db3f5dc1219a7f250eb2c88ec66ebc9c
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-14 08:33:24 -0400

    pulled out postsService.ts and tested

--- src/utils/mocks/mswHandlers.js
+++ src/utils/mocks/mswHandlers.js
@@ -6,4 +6,7 @@ const postsHandler = (req, res, ctx) => {
   return res(ctx.status(200), ctx.json(seedPromotions));
 };
 
-export default [rest.get('*/api/posts', postsHandler)];
+export default [
+  rest.get('*/api/posts', postsHandler),
+  rest.get('http://my-json-server.typicode.com/typicode/demo/posts', postsHandler),
+];
