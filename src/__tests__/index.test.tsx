970fc3296389efed30e2717dd90e41c65481a708
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-14 09:35:03 -0400

    Pulled out Posts.tsx component, reuse posts for both client and server rendering

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -2,34 +2,49 @@ import { render, screen } from '@testing-library/react';
 
 import seedPosts from '../core/mocks/seed/seedPosts';
 import { queryWrapper as wrapper } from '../core/test.utils';
-import Home, { Comment } from '../pages/index';
+import Home from '../pages/index';
+import { Post } from '../services/postsService';
 
 describe('index', () => {
-  const comment: Comment = {
-    id: 1,
-    body: 'bodyValue1',
-  };
-  const tree = <Home comment={comment} />;
+  const posts: Post[] = [
+    {
+      id: 11,
+      title: 'titleValue11',
+    },
+    {
+      id: 12,
+      title: 'titleValue12',
+    },
+  ];
+  const tree = <Home posts={posts} />;
 
   it('should render staticProps', () => {
     // arrange
-    const expectedComment = `id: ${comment.id} - body: ${comment.body}`;
-
-    // act
-    render(tree, { wrapper });
-
-    // assertX
-    screen.getByText(expectedComment);
-  });
-
-  it('should render client-fetched posts', async () => {
-    // arrange
-    const expectedPosts = `Length: ${seedPosts.length}`;
+    const postLength = `Length: ${posts.length}`;
 
     // act
     render(tree, { wrapper });
 
     // assert
-    await screen.findByText(expectedPosts);
+    expect(screen.getByText(postLength)).toBeInTheDocument();
+
+    posts.forEach((post) => {
+      expect(screen.getByText(`${post.id} - ${post.title}`)).toBeInTheDocument();
+    });
+  });
+
+  it('should render client-fetched posts', async () => {
+    // arrange
+    const postLength = `Length: ${seedPosts.length}`;
+
+    // act
+    render(tree, { wrapper });
+
+    // assert
+    expect(await screen.findByText(postLength)).toBeInTheDocument();
+
+    seedPosts.forEach((post) => {
+      expect(screen.getByText(`${post.id} - ${post.title}`)).toBeInTheDocument();
+    });
   });
 });
