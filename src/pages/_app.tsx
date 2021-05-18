de9846937d0c317b02a6e9cc786a58d97474fe58
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-18 13:41:12 -0400

    remove app insights for now

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,13 @@ import '../../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
-import TelemetryProvider from '../components/AppTelemetry/TelemetryProvider';
 import AppState from '../context/AppState';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <TelemetryProvider>
-      <AppState>
-        <Component {...pageProps} />
-      </AppState>
-    </TelemetryProvider>
+    <AppState>
+      <Component {...pageProps} />
+    </AppState>
   );
 }
 
