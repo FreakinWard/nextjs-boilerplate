0e8b331506ae8a1ae0e7f30d6461aad971b65b89
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-18 14:27:06 -0400

    default env key for build, removed unused packages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
+  env: {
+    APPSETTING_APPINSIGHTS_INSTRUMENTATIONKEY: '',
+  },
   publicRuntimeConfig: {
     appInsightsKey: process.env.APPSETTING_APPINSIGHTS_INSTRUMENTATIONKEY,
   },
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
 import '../../styles/globals.css';
 
 import type { AppProps } from 'next/app';
-import getConfig from 'next/config';
 
 import TelemetryProvider from '../components/AppTelemetry/TelemetryProvider';
 import AppState from '../context/AppState';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { publicRuntimeConfig } = getConfig();
-  const appInsightsKey = publicRuntimeConfig.appInsightsKey;
-
   return (
     <TelemetryProvider>
       <AppState>
