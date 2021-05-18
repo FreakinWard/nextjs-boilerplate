87d78826409d9d49a5aa70bf6feaca5e2c67dc64
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-17 23:21:59 -0400

    cleaned up next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',
-//   publicRuntimeConfig: {
-//     appInsightsKey: 'e6c58f4e-ef31-4af7-b218-fe6b5ade0d61',
-//   },
-// });
-//
-// module.exports = withBundleAnalyzer({});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = {
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    mySecret: 'secret',
-    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
-  },
+module.exports = withBundleAnalyzer({
   publicRuntimeConfig: {
-    // Will be available on both server and client
     appInsightsKey: 'e6c58f4e-ef31-4af7-b218-fe6b5ade0d61',
   },
-};
+});
