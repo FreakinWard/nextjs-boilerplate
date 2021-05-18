9808ec70d72301b0a0f8155624eb6071341b7036
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-18 15:22:05 -0400

    removed env and useAppConfig.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 
 import styles from '../../styles/Home.module.css';
 import Posts from '../components/Posts';
-import useAppConfig from '../hooks/useAppConfig';
 import usePosts from '../hooks/usePosts';
 import { fetchPosts, Post } from '../services/postsService';
 
@@ -26,7 +25,6 @@ export async function getStaticProps(): Promise<Props> {
 
 export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   const { data: clientPosts } = usePosts();
-  const { data: appConfig } = useAppConfig();
 
   return (
     <div className={styles.container}>
@@ -43,11 +41,6 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
 
         <Posts title="Server render" posts={posts} />
         <Posts title="Client render" posts={clientPosts} />
-
-        <h3>App Configs</h3>
-        <div style={{ width: 750, overflow: 'scroll', height: 850 }}>
-          <pre>{JSON.stringify(appConfig, null, 2)}</pre>
-        </div>
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.js</code>
