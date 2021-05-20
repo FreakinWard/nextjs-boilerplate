f68214eb9eace8b6f8bde752c738ce2b6797a41f
Aaron Ward <aaron.l.ward@gmail.com>
2021-05-20 08:34:03 -0400

    cleaned up style

--- src/pages/health.tsx
+++ src/pages/health.tsx
@@ -1,19 +1,22 @@
+import styles from '../../styles/Home.module.css';
 import useHealth from '../hooks/useHealth';
 
 export default function Health() {
   const { data: health } = useHealth();
 
   return (
-    <>
-      <h1>Health</h1>
-      <div>
-        <span>Status: </span>
-        <span>{health?.status}</span>
+    <div>
+      <div className={styles.card}>
+        <h2>Health Check</h2>
+        <div>
+          <span>Status: </span>
+          <span>{health?.status}</span>
+        </div>
+        <div>
+          <span>Version: </span>
+          <span>{health?.version}</span>
+        </div>
       </div>
-      <div>
-        <span>Version: </span>
-        <span>{health?.version}</span>
-      </div>
-    </>
+    </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from '../../styles/Home.module.css';
 import Posts from '../components/Posts';
@@ -40,8 +41,20 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <Posts title="Server render" posts={posts} />
-        <Posts title="Client render" posts={clientPosts} />
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <Posts title="Server render" posts={posts} />
+          </div>
+
+          <div className={styles.card}>
+            <Posts title="Client render" posts={clientPosts} />
+          </div>
+
+          <a href="/health" className={styles.card}>
+            <h2>Health Check &rarr;</h2>
+            <p>Access application health data</p>
+          </a>
+        </div>
 
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.js</code>
