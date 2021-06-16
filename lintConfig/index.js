c2d014e0ffd581b098893ebcb5d718a33c2c571e
Aaron Ward <aaron.l.ward@gmail.com>
2021-06-15 23:02:09 -0400

    cleanup

--- lintConfig/index.js
+++ lintConfig/index.js
@@ -13,20 +13,14 @@ module.exports = {
     },
   },
   env: {
-    // browser: true, // Enables browser globals like window and document
-    // amd: true, // Enables require() and define() as global variables as per the amd spec.
     node: true, // Enables Node.js global variables and Node.js scoping.
     jest: true,
   },
   plugins: ['simple-import-sort', 'prettier', 'import'],
   extends: [
     'eslint:recommended',
-    // 'plugin:react/recommended',
-    // 'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    // 'plugin:react-hooks/recommended',
-    // 'plugin:jest/style',
     'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
 
     './rules/react',
@@ -36,26 +30,11 @@ module.exports = {
   rules: {
     'import/prefer-default-export': 'error',
     'prettier/prettier': 'error', // Use our prettier.config.js file as source
-    // 'react/react-in-jsx-scope': 'off',
-    // 'react/prop-types': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'error',
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
-    // 'jsx-a11y/anchor-is-valid': [
-    //   'error',
-    //   {
-    //     components: ['Link'],
-    //     specialLink: ['hrefLeft', 'hrefRight'],
-    //     aspects: ['invalidHref', 'preferButton'],
-    //   },
-    // ],
-    // 'jest/no-disabled-tests': 'warn',
-    // 'jest/no-focused-tests': 'error',
-    // 'jest/no-identical-title': 'error',
-    // 'jest/prefer-to-have-length': 'warn',
-    // 'jest/valid-expect': 'error',
   },
 };
 
