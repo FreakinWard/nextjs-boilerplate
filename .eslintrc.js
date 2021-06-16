931cc90eea6b776f8e5ec4636c04f70e29bb9cd0
Aaron Ward <aaron.l.ward@gmail.com>
2021-06-16 13:10:14 -0400

    installed the local package from file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    'myconfig',
+    'boilerplate',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
