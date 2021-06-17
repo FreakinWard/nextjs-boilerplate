406d00b73ef919b26f5c5e63a2181af7a61e069c
Aaron Ward <aaron.l.ward@gmail.com>
2021-06-16 22:47:15 -0400

    installed eslint-config-react-goodies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-    'boilerplate',
+    'react-goodies',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
