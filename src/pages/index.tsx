c6de0687b18e3ba24056bc5584e16da1980a3672
Aaron Ward <aaron.l.ward@gmail.com>
2021-06-29 23:19:07 -0400

    basic formik

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Field, Form, Formik } from 'formik';
 import { InferGetStaticPropsType } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
@@ -23,6 +24,34 @@ export async function getStaticProps(): Promise<Props> {
     },
   };
 }
+
+const Basic = () => (
+  <div>
+    <h1>Sign Up</h1>
+    <Formik
+      initialValues={{
+        firstName: '',
+        lastName: '',
+        email: '',
+      }}
+      onSubmit={async values => {
+        await new Promise(r => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+      }}>
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field id="firstName" name="firstName" placeholder="Jane" />
+
+        <label htmlFor="lastName">Last Name</label>
+        <Field id="lastName" name="lastName" placeholder="Doe" />
+
+        <label htmlFor="email">Email</label>
+        <Field id="email" name="email" placeholder="jane@acme.com" type="email" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  </div>
+);
 
 export default function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   const { data: clientPosts } = usePosts();
@@ -53,6 +82,10 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
             <h2>Health Check &rarr;</h2>
             <p>Access application health data</p>
           </a>
+
+          <div className={styles.card}>
+            <Basic />
+          </div>
         </div>
 
         <p className={styles.description}>
