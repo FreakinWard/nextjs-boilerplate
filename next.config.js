db8613c8f70c4bb54b7eb5b79554da4fafd80364
freakinward <aaron.l.ward@gmail.com>
2021-12-15 14:06:00 -0500

    disabled swcminify - https://nextjs.org/docs/messages/failed-loading-swc

--- next.config.js
+++ next.config.js
@@ -7,5 +7,5 @@ module.exports = withBundleAnalyzer({
   publicRuntimeConfig: {
     appInsightsConnectionString: process.env.APPLICATIONINSIGHTS_CONNECTION_STRING,
   },
-  swcMinify: true,
+  swcMinify: false,
 });
