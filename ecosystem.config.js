1ac65c1b4c4409bf16854c32fbf30ea7fcaa7359
freakinward <aaron.l.ward@gmail.com>
2022-01-19 20:14:56 -0500

    stages qa and prod - enabled deployments - pm2 script start

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,9 @@ module.exports = {
   apps: [
     {
       name: 'nextjs-boilerplate',
-      // script: 'start',
-      script: './node_modules/next/dist/bin/next',
-      args: 'start:prod -p ' + (process.env.PORT || 3000),
+      script: 'start',
+      // script: './node_modules/next/dist/bin/next',
+      // args: 'start:prod -p ' + (process.env.PORT || 3000),
       watch: false,
       autorestart: false,
     },
