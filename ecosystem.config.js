ee78adc093abb5bbaf8ab73df65683f3a19aaad9
freakinward <aaron.l.ward@gmail.com>
2022-01-21 11:02:46 -0500

    ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'nextjs-boilerplate',
-      script: 'start',
-      // script: './node_modules/next/dist/bin/next',
-      // args: 'start:prod -p ' + (process.env.PORT || 3000),
-      watch: false,
-      autorestart: false,
+      name: 'auctions-checkout-microsite',
+      script: 'npm',
+      args: 'start',
+      instances: 'max',
+      exec_mode: 'cluster',
     },
   ],
 };
