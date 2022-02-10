478fcbe7887343c15b2b5abbc8fb0ac94163de91
AyAyRon <aaron.l.ward@gmail.com>
2022-02-09 23:03:36 -0500

    server appInsights

---
+++ load-appInsights.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const appInsights = require('applicationinsights');
+
+const connectionString = process.env.APPLICATIONINSIGHTS_CONNECTION_STRING;
+
+appInsights
+  .setup(connectionString)
+  .setAutoCollectConsole(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectHeartbeat(true)
+  .setAutoCollectPerformance(true, true)
+  .setAutoCollectRequests(true)
+  .setAutoDependencyCorrelation(true)
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+  .setSendLiveMetrics(true)
+  .setUseDiskRetryCaching(true);
+
+appInsights.defaultClient.setAutoPopulateAzureProperties(true);
+
+const cloudRoleKey = appInsights.defaultClient.context.keys.cloudRole;
+appInsights.defaultClient.context.tags[cloudRoleKey] = process.env.applicationName;
+
+appInsights.start();
