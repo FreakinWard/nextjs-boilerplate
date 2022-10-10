cd09c26813fb372921439dbebbffa9d42b2c1201
Aaron Ward <aaron.l.ward@gmail.com>
2022-10-09 21:59:30 -0500

    azDeploy-resourceGroup.yml

--- cypress/e2e/health.cy.js
+++ cypress/e2e/health.cy.js
@@ -10,13 +10,21 @@ describe('health', () => {
     cy.findByText('Health Check');
   });
 
+  it('should set ciBuildNumber environment variable', () => {
+    // arrange
+    const buildNumber = process.env.CI_BUILD_NUMBER ?? 'not-set';
+
+    // act
+    // assert
+    expect(Cypress.env('CI_BUILD_NUMBER')).to.equal(buildNumber);
+  });
+
   it('should show expected build number', () => {
     // arrange
     const buildNumber = process.env.CI_BUILD_NUMBER ?? 'not-set';
 
     // act
     // assert
-    // cy.findByText(`BuildNumber: ${buildNumber}`);
-    expect(Cypress.env('CI_BUILD_NUMBER')).to.equal(buildNumber);
+    cy.findByText(`BuildNumber: ${buildNumber}`);
   });
 });
