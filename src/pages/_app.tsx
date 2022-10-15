39dbf9b82f6ea7fde7b86915194b4641e6bcaa00
freakinward <aaron.l.ward@gmail.com>
2022-10-15 18:41:43 -0400

    now using next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import '../../styles/globals.css';
 
 import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import { DefaultSession } from 'next-auth';
 
 import AppHead from '../components/AppHead/AppHead';
 import { TelemetryProvider } from '../components/AppTelemetry/TelemetryProvider';
@@ -12,16 +13,17 @@ import AppState from '../context/AppState';
 
 type Page<P = {}, IP = P> = NextPage<P, IP> & {
   title: string;
+  requireAuth: boolean;
 };
 
-type Props<P = {}> = AppProps<P> & {
+type Props<P = {}> = AppProps<{ session: DefaultSession }> & {
   Component: Page<P>;
 };
 
 export default function App({ Component, pageProps, router }: Props) {
   return (
     <TelemetryProvider component={Component} router={router}>
-      <AppState>
+      <AppState requireAuth={Component.requireAuth} session={pageProps.session}>
         <Layout>
           <AppHead title={Component.title} />
           <Component {...pageProps} />
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */ // TODO: determine a way to test
+
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+};
+export default NextAuth(authOptions);
