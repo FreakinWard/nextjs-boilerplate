c6486ba76475e5abe31f5916689ed2f15850db70
freakinward <aaron.l.ward@gmail.com>
2024-11-02 15:43:04 -0500

    fix: msw will now allow app resources to pass through

--- src/core/msw/handlers.ts
+++ src/core/msw/handlers.ts
@@ -14,7 +14,18 @@ const mockRequestGet = (url, responseData, statusCode = 200) => {
   return rest.get(url, mockHandler(responseData, statusCode));
 };
 
+export const mockPassThroughPost = (url: string) => rest.post(url, req => req.passthrough());
+export const mockPassThroughGet = (url: string) => rest.get(url, req => req.passthrough());
+
 export default [
+  // app - expected pass through
+  mockPassThroughPost('*/track'),
+  mockPassThroughPost('*/QuickPulseService.svc/post'),
+
+  mockPassThroughGet('*.svg'),
+  mockPassThroughGet('*.ico'),
+  mockPassThroughGet('/_next/*'),
+
   // auth
   mockRequestGet('*/api/auth/session', seedAuth.session),
   mockRequestGet('*/api/auth/providers', seedAuth.providers),
